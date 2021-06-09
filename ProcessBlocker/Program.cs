2d06ae6ab062843bc4fe5dfdb956370bc7b63b52
xsucculentx <60blackandyellow60@gmail.com>
2021-06-08 17:25:42 -0700

    Added partial functionality

--- ProcessBlocker/Program.cs
+++ ProcessBlocker/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Diagnostics;
 
+using System.IO;
 namespace ProcessBlocker
 {
     class Program
     {
+        public const double Version = 0.1;
+        public const string Branch = "alpha";
+
+        public static string[] ConfigFileData;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"ProcessBlocker v{Version}-{Branch}");
+            Console.WriteLine("FOR BEST EXPERIENCE RUN WITH SUDO/ADMIN");
+            Console.Write("Processing Config... ");
+            
+            if (OperatingSystem.IsWindows())
+            {
+                ConfigFileData = File.ReadAllLines($@"C:\Users\{Environment.UserName}\.processblocker");
+            }
+            if (OperatingSystem.IsLinux())
+            {
+                ConfigFileData = File.ReadAllLines($@"/home/{Environment.UserName}/.config/processblocker");
+            }
+            else
+            {
+                Console.Write("Done.\n");
+                Console.WriteLine("Can't detect operating system.");
+                Environment.Exit(1);
+            }
+            
+            Console.Write("Done.\n");
+            
+            BlockProcesses();
+        }
+
+        static void BlockProcesses()
+        {
+            Console.WriteLine("Searching for processes...");
+            
+            foreach (string processName in ConfigFileData)
+            {
+                while (true)
+                {
+                    Process[] processes = Process.GetProcessesByName(processName);
+                    foreach (Process process in processes)
+                    {
+                        if (!process.HasExited)
+                        {
+                            Console.WriteLine($"Killing {process.Handle} ({process.Id})");
+                            process.Kill();
+                        }
+                    }
+                }
+            }
         }
     }
 }
